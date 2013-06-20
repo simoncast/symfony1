e1f77c6173855b6ce3e1a7de337030ad030d07f2
Simon Cast <simon.cast@gmail.com>
2013-06-20 21:54:55 +0100

    finally found a solution to the remaining array to string conversion problems

--- lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Query/Abstract.php
+++ lib/plugins/sfDoctrinePlugin/lib/vendor/doctrine/Doctrine/Query/Abstract.php
@@ -1149,9 +1149,10 @@ abstract class Doctrine_Query_Abstract
         $copy->free();
 
         if ($componentsBefore !== $componentsAfter) {
-            return array_diff_assoc($componentsAfter, $componentsBefore);
+          return $this->array_diff_assoc_recursive($componentsAfter,$componentsBefore);                
+          //return array_diff_assoc($componentsAfter, $componentsBefore);
         } else {
-            return $componentsAfter;
+          return $componentsAfter;
         }
     }
 
@@ -2167,5 +2168,29 @@ abstract class Doctrine_Query_Abstract
     public function setDisableLimitSubquery($disableLimitSubquery)
     {
         $this->disableLimitSubquery = $disableLimitSubquery;
+    }
+
+    protected function array_diff_assoc_recursive($array1,$array2)
+    {
+      $difference = array();
+      foreach($array1 as $key => $value) 
+      {
+        if( is_array($value) ) 
+        {
+          if( !isset($array2[$key]) || !is_array($array2[$key]) ) 
+          {
+            $difference[$key] = $value;
+          } else {
+            $new_diff = $this->array_diff_assoc_recursive($value, $array2[$key]);
+            if( !empty($new_diff) ) 
+            {
+              $difference[$key] = $new_diff;
+            }
+          }
+        } else if( !array_key_exists($key,$array2) || $array2[$key] !== $value ) {
+            $difference[$key] = $value;
+        }
+      }
+      return $difference;        
     }
 }
